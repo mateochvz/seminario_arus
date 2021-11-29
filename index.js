cd178350878365d9f5f4c74d3660e73bbe3c5c81
sala 305 <sala305@poliacademico.edu.loc>
2021-11-29 07:50:44 -0500

    express dependencies in index.js added

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const chalk = require('chalk')
+const express = require('express')
 
-console.log(chalk.bgCyan.italic.bold('Hello world!'));
+const app = express()
+
+let port =
+app.listen(3000, ()=>{
+    console.log('Servidor corriendo en el puerto',3000)
+})
+
+// console.log(chalk.bgCyan.italic.bold('Hello world!'));
