b7ce1789b0a959a9e6ff011f0de037cfe662d38c
sala 305 <sala305@poliacademico.edu.loc>
2021-12-06 07:52:28 -0500

    Uso de metodos get post con Nodejs y Postman

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 // const chalk = require('chalk')
 const express = require('express')
+const x=[]
 
 const app = express()
 
 app.get('/home', (req, res) => {res.send('<h1>Hola mundo </h1>')})
 app.get('/page', (req, res) => {res.send({message: 'Hola mundo'})})
+app.post('/user',express.json(),(req, res)=>{
+  console.log(req.body)
+  x.push(req.body)
+  res.send({mensaje: 'este es el post'})
+})
+app.get('/user',(req,res)=>{
+  res.send({users:x})
+})
 
 const port = 3000
 app.listen(port, () => {
